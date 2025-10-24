62b5a97bbb8a2493ebedfec475b53e9745f8d874
Nur Hidayah <35320146+nrhdyh@users.noreply.github.com>
2025-10-24 15:07:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import streamlit as st
 
+st.set_page_config(page_title="Climate Smart Agriculture Dashboard", layout="wide")
 
-st.set_page_config(
-    page_title="Student Survey"
-)
+# Pages
+home = st.Page("home.py", title="🏠 Home", default=True)
+objective1 = st.Page("objective1.py", title="🎓 Objective 1: Education & Demographics")
+objective2 = st.Page("objective2.py", title="🌾 Objective 2: Land & Economic Factors")
+objective3 = st.Page("objective3.py", title="🌱 Objective 3: Climate-Smart Practices")
 
-visualise = st.Page('objective1.py', title='Objective 1')
-
-home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
-
-pg = st.navigation(
-        {
-            "Menu": [home, visualise]
-        }
-    )
+# Navigation
+pg = st.navigation({
+    "Main Menu": [home, objective1, objective2, objective3]
+})
 
 pg.run()
