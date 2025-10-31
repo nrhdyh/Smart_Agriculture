04e12e184cb25325a4dfd07f6382e04bc44a40c1
Nur Hidayah <35320146+nrhdyh@users.noreply.github.com>
2025-10-31 14:38:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ st.set_page_config(page_title="Climate Smart Agriculture Dashboard", layout="wid
 # Pages
 home = st.Page("home.py", title="🏠 Home", default=True)
 objective1 = st.Page("objective1.py", title="🎓 Objective 1: Education & Demographics")
-objective2 = st.Page("objective2.py", title="🌾 Objective 2: Land & Economic Factors")
-objective3 = st.Page("objective3.py", title="🌱 Objective 3: Climate-Smart Practices")
+objective2 = st.Page("objective2.py", title="🌾 Objective 2: Land & Perception")
+objective3 = st.Page("objective3.py", title="🌱 Objective 3: Practices & Correlation")
 
 # Navigation
 pg = st.navigation({
