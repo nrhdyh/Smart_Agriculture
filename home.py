18704e23a428ee7bb2e97612d03773158e5e3195
Nur Hidayah <35320146+nrhdyh@users.noreply.github.com>
2025-11-01 12:28:54 +0800

    Update home.py

--- home.py
+++ home.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import time
-import plotly.graph_objects as go
 # ===========================
 # LOAD DATA DIRECTLY FROM GITHUB
 # ===========================
